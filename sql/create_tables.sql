77c34dc168e2b8a9a21f77e0358caa7ad306fca8
Manu E Thomas <manuethomas30@gmail.com>
2025-02-02 17:23:39 +0530

    modified tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -13,8 +13,8 @@ CREATE TABLE fact_orders (
     eval_set VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS departments;
-CREATE TABLE departments (
+DROP TABLE IF EXISTS dim_departments;
+CREATE TABLE dim_departments (
     department_id INT PRIMARY KEY,
     department VARCHAR(255)
 );
@@ -31,7 +31,7 @@ CREATE TABLE dim_products (
     product_name VARCHAR(255),
     aisle_id INT,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    FOREIGN KEY (department_id) REFERENCES dim_departments(department_id),
     FOREIGN KEY (aisle_id) REFERENCES dim_aisles(aisle_id)
 );
 
@@ -41,7 +41,9 @@ CREATE TABLE fact_order_products (
     product_id INT,
     add_to_cart_order INT,
     reordered INT,
-    PRIMARY KEY (order_id, product_id)
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES dim_products(product_id)
 );
 
 DROP TABLE IF EXISTS fact_order_products_prior;
@@ -50,6 +52,7 @@ CREATE TABLE fact_order_products_prior (
     product_id INT,
     add_to_cart_order INT,
     reordered INT,
-    PRIMARY KEY (order_id, product_id)
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES dim_products(product_id)
 );
-
