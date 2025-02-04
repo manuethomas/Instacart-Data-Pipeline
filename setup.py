e3e04ed43dd3bbfc9040ee0b48101b00319ed955
Manu E Thomas <manuethomas30@gmail.com>
2025-02-04 15:36:04 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='instacart-data-pipeline',
-    version='0.1',
-    package_dir={'': 'src'},
+    name='instacart_data_pipeline',
+    version='0.1.0',
     packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
         'pandas',
         'sqlalchemy',
         'psycopg2',
-        'apache-airflow',
-        'docker',
-        'pytest'
+        #'apache-airflow',
+        #'docker',
+        'pytest',
+        'pyyaml'
     ],
+    entry_points={
+        'console_scripts': [
+            'run_pipeline=src.main:main',
+        ],
+    },
 )
-
