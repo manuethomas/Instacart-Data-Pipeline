93d660caa8b8eab657a64db9512f4ead573977fc
Manu E Thomas <manuethomas30@gmail.com>
2025-02-07 21:14:58 +0530

    create tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,32 +1,26 @@
--- Create Database
--- CREATE DATABASE insta_cart_db
 
 -- Create tables
-DROP TABLE IF EXISTS fact_orders;
-CREATE TABLE fact_orders (
+CREATE TABLE IF NOT EXISTS fact_orders (
     order_id INT PRIMARY KEY,
     user_id INT,
+    eval_set VARCHAR(255),
     order_number INT,
     order_dow INT,
     order_hour_of_day INT,
-    days_since_prior_order INT,
-    eval_set VARCHAR(255)
+    days_since_prior_order INT
 );
 
-DROP TABLE IF EXISTS dim_departments;
-CREATE TABLE dim_departments (
+CREATE TABLE IF NOT EXISTS dim_departments (
     department_id INT PRIMARY KEY,
     department VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS dim_aisles;
 CREATE TABLE dim_aisles (
     aisle_id INT PRIMARY KEY,
     aisle VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS dim_products;
-CREATE TABLE dim_products (
+CREATE TABLE IF NOT EXISTS dim_products (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(255),
     aisle_id INT,
@@ -35,24 +29,20 @@ CREATE TABLE dim_products (
     FOREIGN KEY (aisle_id) REFERENCES dim_aisles(aisle_id)
 );
 
-DROP TABLE IF EXISTS fact_order_products;
-CREATE TABLE fact_order_products (
+CREATE TABLE IF NOT EXISTS fact_order_products (
     order_id INT,
     product_id INT,
     add_to_cart_order INT,
     reordered INT,
-    PRIMARY KEY (order_id, product_id),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
     FOREIGN KEY (product_id) REFERENCES dim_products(product_id)
 );
 
-DROP TABLE IF EXISTS fact_order_products_prior;
-CREATE TABLE fact_order_products_prior (
+CREATE TABLE IF NOT EXISTS fact_order_products_prior (
     order_id INT,
     product_id INT,
     add_to_cart_order INT,
     reordered INT,
-    PRIMARY KEY (order_id, product_id),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
     FOREIGN KEY (product_id) REFERENCES dim_products(product_id)
 );
